ea34401c261022788d49d2fe75b5251214ec3f43
lachlanking <129047556+lachlanking@users.noreply.github.com>
2024-01-18 12:54:40 +1100

    Time: 14 ms (86.27%), Space: 19.6 MB (38.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+class Solution {
+public:
+    void moveZeroes(std::vector<int>& nums) {
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        for (int i = nonZeroIndex; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
