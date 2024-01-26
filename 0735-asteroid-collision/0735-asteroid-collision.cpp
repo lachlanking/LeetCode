ee93efefc768e2f520125f6f59fd38689e6f634f
lachlanking <129047556+lachlanking@users.noreply.github.com>
2024-01-26 22:35:31 +1100

    Time: 10 ms (66.11%), Space: 20.1 MB (8.93%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> result;
+        for (int asteroid : asteroids) {
+            if (result.empty() || asteroid > 0) {
+                result.push_back(asteroid);
+            } else {
+                while (!result.empty() && result.back() > 0 && result.back() < abs(asteroid)) {
+                    result.pop_back();
+                }
+                if (result.empty() || result.back() < 0) {
+                    result.push_back(asteroid);
+                } else if (result.back() == abs(asteroid)) {
+                    result.pop_back();
+                }
+            }
+        }
+        return result;
+    }
+};
